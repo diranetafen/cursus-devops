0241331e842a1d65fd1699691e714e583391b89c
Dirane TAFEN <diranetafen@yahoo.com>
2021-07-22 09:50:18 +0200

    move some variables in vagrantfile

--- vagrant/kubernetes/install_kubernetes.sh
+++ vagrant/kubernetes/install_kubernetes.sh
@@ -8,9 +8,14 @@ yum -y install ansible
 # retrieve ansible code
 yum -y install git
 rm -Rf cursus-devops || echo "previous folder removed"
-git clone -b kubernetes https://github.com/diranetafen/cursus-devops.git
+git clone -b kubernetes-var https://github.com/diranetafen/cursus-devops.git
 cd cursus-devops/ansible
 ansible-galaxy install -r roles/requirements.yml
-ansible-playbook install_kubernetes.yml --extra-vars "kubernetes_role=$1"
+if [ $1 == "master" ]
+then
+        ansible-playbook install_kubernetes.yml --extra-vars "kubernetes_role=$1 kubernetes_apiserver_advertise_address=$2"
+else
+        ansible-playbook install_kubernetes.yml --extra-vars "kubernetes_role=$1 kubernetes_apiserver_advertise_address=$2 kubernetes_join_command='kubeadm join {{ kubernetes_apiserver_advertise_address }}:6443 --ignore-preflight-errors=all --token={{ token }}  --discovery-token-unsafe-skip-ca-verification'"
+fi
 #sudo usermod -aG docker vagrant
 echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
