f2beb6a78b1bc57b1a4dda30b3bda5303e40bc60
Dirane TAFEN <diranetafen@yahoo.com>
2021-09-15 21:28:12 +0200

    Add caution for running minikube start

--- vagrant/minikube/install_minikube.sh
+++ vagrant/minikube/install_minikube.sh
@@ -18,4 +18,4 @@ sudo mv kubectl  /usr/bin/
 sudo echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 sudo systemctl enable docker.service
 echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
-echo "Connect through ssh on the VM and then run 'minikube start --driver=none'"
+echo "Connect through ssh on the VM and then run 'minikube start --driver=none' please don't run the command as root user or using any sudo command, use vagrant user"
