76914f802721612fbb38f4e3bc55b29490492957
Ulrich MONJI <ulrich_nathan@yahoo.fr>
2022-02-28 00:03:27 +0100

    Install docker-compose
    Add instruction to install docker compose on provisioning script used to install docker VM

--- vagrant/docker/install_docker.sh
+++ vagrant/docker/install_docker.sh
@@ -7,4 +7,6 @@ sh get-docker.sh
 sudo usermod -aG docker vagrant
 sudo systemctl enable docker
 sudo systemctl start docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
