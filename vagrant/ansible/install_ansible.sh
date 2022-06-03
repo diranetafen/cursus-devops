b4576c8c2903f65768cb345cc8b8c6ad2df6438e
Ulrich MONJI <ulrich_nathan@yahoo.fr>
2022-06-03 17:12:42 +0200

    add zsh on docker, ansible and Minikube stack

--- vagrant/ansible/install_ansible.sh
+++ vagrant/ansible/install_ansible.sh
@@ -10,5 +10,19 @@ then
   curl -sS https://bootstrap.pypa.io/pip/3.6/get-pip.py | sudo python3
   /usr/local/bin/pip3 install ansible
   yum install -y sshpass
+  
+  # Install zsh if needed
+  if [ $ENABLE_ZSH == "true" ]
+    then
+      echo "We are going to install zsh"
+      sudo yum -y install zsh git
+      echo "vagrant" | chsh -s /bin/zsh vagrant
+      su - vagrant  -c  'echo "Y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+      su - vagrant  -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+      sed -i 's/^plugins=/#&/' /home/vagrant/.zshrc
+      echo "plugins=(git  colored-man-pages aliases copyfile  copypath zsh-syntax-highlighting jsontools)" >> /home/vagrant/.zshrc
+      sed -i "s/^ZSH_THEME=.*/ZSH_THEME='agnoster'/g"  /home/vagrant/.zshrc
+  fi
+
 fi
 echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
--- vagrant/minikube/install_minikube.sh
+++ vagrant/minikube/install_minikube.sh
@@ -17,9 +17,22 @@ sudo chmod +x kubectl
 sudo mv kubectl  /usr/bin/
 sudo echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables
 sudo systemctl enable docker.service
-su - vagrant -c "minikube start --driver=none"
+su - vagrant -c "minikube start --kubernetes-version=1.20.0 --driver=none"
 yum install bash-completion -y
 echo 'source <(kubectl completion bash)' >> ~vagrant/.bashrc
 echo 'alias k=kubectl' >> ~vagrant/.bashrc
 echo 'complete -F __start_kubectl k' >> ~vagrant/.bashrc
+
+if [ $ENABLE_ZSH == "true" ]
+then
+    echo "We are going to install zsh"
+    sudo yum -y install zsh git
+    echo "vagrant" | chsh -s /bin/zsh vagrant
+    su - vagrant  -c  'echo "Y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+    su - vagrant  -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    sed -i 's/^plugins=/#&/' /home/vagrant/.zshrc
+    echo "plugins=(git docker docker-compose helm kubectl kubectx minikube colored-man-pages aliases copyfile  copypath dotenv zsh-syntax-highlighting jsontools)" >> /home/vagrant/.zshrc
+    sed -i "s/^ZSH_THEME=.*/ZSH_THEME='agnoster'/g"  /home/vagrant/.zshrc
+fi
+
 echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
