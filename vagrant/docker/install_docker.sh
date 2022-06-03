3bbd31fdb3a5b3fb6886df1af8f07c97cfc8ae46
Ulrich MONJI <ulrich_nathan@yahoo.fr>
2022-06-04 00:51:38 +0200

    fixing

--- vagrant/docker/install_docker.sh
+++ vagrant/docker/install_docker.sh
@@ -11,7 +11,7 @@ sudo systemctl start docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-if [ $ENABLE_ZSH == "true" ]
+if [[ !(-z "$ENABLE_ZSH")  &&  ($ENABLE_ZSH == "true") ]]
 then
     echo "We are going to install zsh"
     sudo yum -y install zsh git
@@ -21,6 +21,8 @@ then
     sed -i 's/^plugins=/#&/' /home/vagrant/.zshrc
     echo "plugins=(git  docker docker-compose colored-man-pages aliases copyfile  copypath dotenv zsh-syntax-highlighting jsontools)" >> /home/vagrant/.zshrc
     sed -i "s/^ZSH_THEME=.*/ZSH_THEME='agnoster'/g"  /home/vagrant/.zshrc
+  else
+    echo "Provisionning not Enabled"    
 fi
 
 echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
--- vagrant/minikube/install_minikube.sh
+++ vagrant/minikube/install_minikube.sh
@@ -23,7 +23,7 @@ echo 'source <(kubectl completion bash)' >> ~vagrant/.bashrc
 echo 'alias k=kubectl' >> ~vagrant/.bashrc
 echo 'complete -F __start_kubectl k' >> ~vagrant/.bashrc
 
-if [ $ENABLE_ZSH == "true" ]
+if [[ !(-z "$ENABLE_ZSH")  &&  ($ENABLE_ZSH == "true") ]]
 then
     echo "We are going to install zsh"
     sudo yum -y install zsh git
@@ -33,6 +33,8 @@ then
     sed -i 's/^plugins=/#&/' /home/vagrant/.zshrc
     echo "plugins=(git docker docker-compose helm kubectl kubectx minikube colored-man-pages aliases copyfile  copypath dotenv zsh-syntax-highlighting jsontools)" >> /home/vagrant/.zshrc
     sed -i "s/^ZSH_THEME=.*/ZSH_THEME='agnoster'/g"  /home/vagrant/.zshrc
+else
+    echo "Provisionning not Enabled"    
 fi
 
 echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
