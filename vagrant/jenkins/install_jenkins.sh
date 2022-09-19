390b3f2a4f022111e28e9b6819ea0f6b08ed8dd0
Ulrich MONJI <ulrich_nathan@yahoo.fr>
2022-09-19 23:04:33 +0200

    fix zsh
    Fix zsh message when option is not enabled
    add zsh oition on jenkins stack

--- vagrant/jenkins/install_jenkins.sh
+++ vagrant/jenkins/install_jenkins.sh
@@ -14,4 +14,18 @@ ansible-playbook install_docker.yml
 sudo usermod -aG docker vagrant
 cd ../jenkins
 /usr/local/bin/docker-compose up -d
+
+if [[ !(-z "$ENABLE_ZSH")  &&  ($ENABLE_ZSH == "true") ]]
+then
+    echo "We are going to install zsh"
+    sudo yum -y install zsh git
+    echo "vagrant" | chsh -s /bin/zsh vagrant
+    su - vagrant  -c  'echo "Y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+    su - vagrant  -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    sed -i 's/^plugins=/#&/' /home/vagrant/.zshrc
+    echo "plugins=(git docker docker-compose helm kubectl kubectx minikube colored-man-pages aliases copyfile  copypath dotenv zsh-syntax-highlighting jsontools)" >> /home/vagrant/.zshrc
+    sed -i "s/^ZSH_THEME=.*/ZSH_THEME='agnoster'/g"  /home/vagrant/.zshrc
+else
+    echo "The zsh is not installed on this server"
+fi
 echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
