d6a5bd8f358b39829db8db90f187fa6345f84e88
ulrich-sun <ulrichnoumsi98@gmail.com>
2024-01-11 10:14:15 +0100

    Add ArgoCD Stack

--- vagrant/argoCD/install_argocd.sh
+++ vagrant/argoCD/install_argocd.sh
@@ -19,6 +19,9 @@ sudo setenforce 0
 echo 
 echo ...Installing Git
 echo 
+sudo yum -y remove git
+sudo yum -y remove git-*
+sudo yum -y install https://packages.endpointdev.com/rhel/7/os/x86_64/endpoint-repo.x86_64.rpm
 sudo yum install git git-core -y 
 
 #Jenkins Installation
