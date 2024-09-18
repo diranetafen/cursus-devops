d24d40f59dce7aba4e49972715de6e52e3903f98
OlivierKouokam <62020582+OlivierKouokam@users.noreply.github.com>
2024-09-18 09:39:23 +0200

    Update install_minikube.sh

--- vagrant/minikube/install_minikube.sh
+++ vagrant/minikube/install_minikube.sh
@@ -113,3 +113,32 @@ sudo systemctl enable --now cri-docker.socket
 
 # Démarrer Minikube
 minikube start --driver=none
+
+yum install bash-completion -y
+echo 'source <(kubectl completion bash)' >> ~vagrant/.bashrc
+echo 'alias k=kubectl' >> ~vagrant/.bashrc
+echo 'complete -F __start_kubectl k' >> ~vagrant/.bashrc
+
+if [[ !(-z "$ENABLE_ZSH")  &&  ($ENABLE_ZSH == "true") ]]
+then
+    echo "We are going to install zsh"
+    sudo yum -y install zsh git
+    echo "vagrant" | chsh -s /bin/zsh vagrant
+    su - vagrant  -c  'echo "Y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+    su - vagrant  -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+    sed -i 's/^plugins=/#&/' /home/vagrant/.zshrc
+    echo "plugins=(git docker docker-compose helm kubectl kubectx minikube colored-man-pages aliases copyfile  copypath dotenv zsh-syntax-highlighting jsontools)" >> /home/vagrant/.zshrc
+    sed -i "s/^ZSH_THEME=.*/ZSH_THEME='agnoster'/g"  /home/vagrant/.zshrc
+else
+    echo "The zsh is not installed on this server"
+fi
+
+
+IS_MINIKUBE_UP=$(curl -k https://localhost:8443/livez?verbose | grep -i "livez check passed")
+
+if [[ ($IS_MINIKUBE_UP == "livez check passed") ]]
+then
+    echo -e "Everything is Good, minikube is ready. \nFor this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
+else
+    echo "Error, your minikube server (Kubernetes) is not running"
+fi
