00ee95a04cf1ba08e5bb6d1ddce787a1e82d982e
OlivierKouokam <62020582+OlivierKouokam@users.noreply.github.com>
2024-11-14 14:44:34 +0100

    Update install_argocd.sh

--- vagrant/argoCD/install_argocd.sh
+++ vagrant/argoCD/install_argocd.sh
@@ -4,7 +4,7 @@ ENABLE_ZSH=true
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl -y
+sudo apt-get install apt-transport-https ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
