fa3aad4ddd8b7cfc4392327f6642059d293e64b5
NOUMSI ULRICH STEVE <38357057+ulrich-sun@users.noreply.github.com>
2025-03-21 16:31:32 +0100

    new minishift

--- vagrant/minishift/minishift_centos7/install_minishift.sh
+++ vagrant/minishift/minishift_centos7/install_minishift.sh
@@ -2,22 +2,22 @@
 yum -y update
 yum -y install epel-release
 
-if [ $1 == "minishift" ]
+if [ $1 == "minishifts" ]
 then
         echo "###################################################"
         echo "Install minishift"
         echo "###################################################"
-        curl -L https://github.com/minishift/minishift/releases/download/v1.33.0/minishift-1.33.0-linux-amd64.tgz -o minishift.tar.gz
+        curl -L https://github.com/minishift/minishift/releases/download/v1.34.3/minishift-1.34.3-linux-amd64.tgz -o minishift.tar.gz
         tar -xvzf minishift.tar.gz
-        cd minishift-1.33.0-linux-amd64
+        cd minishift-1.34.3-linux-amd64
         cp minishift /usr/bin/
         export PATH=$PWD:$PATH
         export MINISHIFT_ENABLE_EXPERIMENTAL=y
         minishift addons disable minishift-mobilecore-addon
         minishift delete
         rm -rf ~/.minishift/machines
-        minishift start --vm-driver generic --remote-ipaddress $2 --remote-ssh-user vagrant --remote-ssh-key /home/vagrant/.ssh/minishift.key
-        cp /root/.minishift/cache/oc/v3.11.0/linux/oc /usr/bin/
+        minishift start --vm-driver generic --remote-ipaddress $2 --remote-ssh-user vagrant --remote-ssh-key /home/vagrant/.ssh/minishift.key --openshift-version v3.10.0
+        cp /root/.minishift/cache/oc/v3.10.0/linux/oc /usr/bin/
 
         echo "###################################################"
         echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
@@ -38,5 +38,9 @@ else
         systemctl enable docker || true
 
         yum install -y sshpass
+        SSH_CONFIG="/etc/ssh/sshd_config"
+        sudo sed -i 's/^#\s*\(PubkeyAuthentication\s.*\)/\1/' "$SSH_CONFIG"
+        sudo systemctl restart sshd
+        echo "La ligne 'PubkeyAuthentication yes' a été décommentée et le service SSH a été redémarré."
         echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
 fi
