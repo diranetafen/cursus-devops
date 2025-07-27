8d445a280dabcff9b38a468a5b644b12691b9db1
OlivierKouokam <62020582+OlivierKouokam@users.noreply.github.com>
2025-07-27 02:29:50 +0200

    Update install_crc.sh

--- vagrant/code ready container/crc_ubuntu20/install_crc.sh
+++ vagrant/code ready container/crc_ubuntu20/install_crc.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
-sudo sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
-sudo sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
-sudo sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
-yum -y update
 
-# install usefull tools
-yum install -y git wget
+set -e
 
-# install libvirt
-yum install -y qemu-kvm libvirt libvirt-python libguestfs-tools virt-install
-systemctl enable libvirtd
-systemctl start libvirtd
+# Mise à jour des dépôts et upgrade système
+sudo apt update && sudo apt -y upgrade
 
-# download secret
-curl https://eazytraining.fr/wp-content/uploads/2022/10/openshift-crc-pull-secret.txt > /root/pull-secret.txt
+# Installation des outils de base
+sudo apt install -y curl wget git libvirt-daemon-system libvirt-clients qemu-kvm virtinst bridge-utils unzip net-tools
 
-# download crc
-wget https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/crc/2.9.0/crc-linux-amd64.tar.xz
-tar -xf crc-linux-amd64.tar.xz
-cp ./crc-linux-2.9.0-amd64/crc /usr/bin/ && chmod +x /usr/bin/crc
+# Vérification que libvirtd est actif
+sudo systemctl enable --now libvirtd
+
+# Ajout de l’utilisateur vagrant au groupe libvirt
+sudo usermod -aG libvirt vagrant
+
+# Téléchargement de la pull secret
+curl -o /root/pull-secret.txt https://eazytraining.fr/wp-content/uploads/2022/10/openshift-crc-pull-secret.txt
+
+# Téléchargement de CRC
+CRC_VERSION="2.9.0"
+CRC_ARCHIVE="crc-linux-amd64.tar.xz"
+CRC_URL="https://developers.redhat.com/content-gateway/file/pub/openshift-v4/clients/crc/${CRC_VERSION}/${CRC_ARCHIVE}"
+
+wget $CRC_URL
+tar -xf $CRC_ARCHIVE
+sudo cp crc-linux-${CRC_VERSION}-amd64/crc /usr/local/bin/
+sudo chmod +x /usr/local/bin/crc
+
+# Configuration de CRC
 crc config set pull-secret-file /root/pull-secret.txt
 crc config set consent-telemetry yes
 crc config set skip-check-root-user true
@@ -35,18 +44,23 @@ crc start
 # oc new-app openshift/nginx-example -p NAME=webapp -p NGINX_VERSION=1.20-ubi9
 # oc delete project webapp
 
-if [[ !(-z "$ENABLE_ZSH")  &&  ($ENABLE_ZSH == "true") ]]
-then
-    echo "We are going to install zsh"
-    yum -y install zsh git
-    echo "vagrant" | chsh -s /bin/zsh vagrant
-    su - vagrant  -c  'echo "Y" | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
-    su - vagrant  -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+# (Optionnel) Configuration de Zsh si ENABLE_ZSH=true
+if [[ ! -z "$ENABLE_ZSH" && "$ENABLE_ZSH" == "true" ]]; then
+    echo "Installation de Zsh et Oh My Zsh"
+    sudo apt install -y zsh git
+    echo "vagrant" | chsh -s /usr/bin/zsh vagrant
+    su - vagrant -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
+    su - vagrant -c "git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
     sed -i 's/^plugins=/#&/' /home/vagrant/.zshrc
-    echo "plugins=(git  docker docker-compose colored-man-pages aliases copyfile  copypath dotenv zsh-syntax-highlighting jsontools)" >> /home/vagrant/.zshrc
+    echo "plugins=(git docker docker-compose colored-man-pages aliases copyfile copypath dotenv zsh-syntax-highlighting jsontools)" >> /home/vagrant/.zshrc
     sed -i "s/^ZSH_THEME=.*/ZSH_THEME='agnoster'/g"  /home/vagrant/.zshrc
-  else
-    echo "The zsh is not installed on this server"    
+else
+    echo "Zsh ne sera pas installé."
 fi
 
-echo "For this Stack, you will use $(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p') IP Address"
+# (Optionnel) Affichage de l’IP de la VM
+# IP=$(ip -4 addr show enp0s8 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+# echo "L'IP privée de cette VM est : $IP"
+
+IP=$(ip -f inet addr show enp0s8 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')
+echo "For this Stack, you will use $IP IP Address"
